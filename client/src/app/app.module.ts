245553db65c00885b2fe49b70b4243bfcfacbdfa
STRUCTUREIT\kuzivakwashe.muvezwa <kuzivakwashe.muvezwa@structureit.net>
2018-02-20 14:54:28 +0200

    adding updates as of 20/02/2018

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,21 +3,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 import { AlertComponent } from './alert/alert.component';
 
+const routes: Routes = [
+  {path:'', pathMatch: 'full', redirectTo: 'home' },
+  {path:'home', component: HomeComponent},
+  {path:'login', component: LoginComponent}
+];
+
 @NgModule({
-  
 	declarations: [
     AppComponent,
     TodoListComponent,
     TodoDetailComponent,
-    AlertComponent
+    AlertComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule, 
     HttpClientModule,
     HttpModule,
